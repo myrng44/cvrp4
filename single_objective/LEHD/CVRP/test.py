369db6572ee3cde69f03bc07690690e9c82f6b43
myrdin44 <nv.bac44@gmail.com>
2026-03-29 22:40:36 +0700

    fix: update RRC budget and test parameters for '3000to7000' problem size

--- single_objective/LEHD/CVRP/test.py
+++ single_objective/LEHD/CVRP/test.py
@@ -24,7 +24,7 @@ problem_size = '3000to7000'
 Use_RRC = True
 
 # RRC budget
-RRC_budget = 10 #10
+RRC_budget = 40 #10
 
 ########### model ###############
 model_load_path = 'result/20260322_032817_train'
@@ -40,7 +40,7 @@ test_paras = {
     200: ['vrp200_test_lkh.txt', 128, 128],
     500: ['vrp500_test_lkh.txt', 128, 128],
     1000: ['vrp1000_test_lkh.txt', 128, 128],
-    '3000to7000': ['vrp3k4k6k7k_instances.txt', 1, 1]
+    '3000to7000': ['vrp3k4k6k7k_instances.txt', 4, 1]
 }
 
 
